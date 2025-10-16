43cda87f3d151090c97bc21999292e9de244962d
AmaAde <Amalia.Adetu@softwire.com>
2025-10-16 17:43:52 +0300

    Implemented different ways to handle csv/json

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,38 +1,69 @@
-﻿using System.Globalization;
-using System.IO;
+﻿using Newtonsoft.Json;
+
+public class Transaction
+{
+    public DateTime Date { get; set; }
+    public string FromAccount { get; set; }
+    public string ToAccount { get; set; }
+    public string Narrative { get; set; }
+    public decimal Amount { get; set; }
+}
 
 class SupportBank
 {
     static void Main()
     {
+        Console.WriteLine("File to import:\n");
+        string file = Console.ReadLine();
+        List<Transaction> transactions = new List<Transaction>();
+
+        if (file.EndsWith("json"))
+        {
+            string input = File.ReadAllText("Transactions2013.json");
+            transactions = JsonConvert.DeserializeObject<List<Transaction>>(input);
+        }
+        else if (file.EndsWith("csv"))
+        {
+            var lines = File.ReadAllLines(file);
+            bool isFirstLine = true;
+            foreach (var line in lines)
+            {
+                if (isFirstLine)
+                {
+                    isFirstLine = false;
+                    continue;
+                }
+                
+                var fields = line.Split(',');
+                transactions.Add(new Transaction
+                {
+                    Date = DateTime.Parse(fields[0]),
+                    FromAccount = fields[1],
+                    ToAccount = fields[2],
+                    Narrative = fields[3],
+                    Amount = decimal.Parse(fields[4])
+                });
+            }
+        }
+        
+        
         Dictionary<string, Double> accounts = new Dictionary<string, Double>();
         
-        using var reader = new StreamReader("Transactions2014.csv");
-        string line;
-        bool isFirstLine = true;
-        while ((line = reader.ReadLine()) != null)
+        foreach (Transaction transaction in transactions)
         {
-            var fields = line.Split(',');
-            if (isFirstLine)
+            if (!accounts.ContainsKey(transaction.FromAccount))
             {
-                isFirstLine = false;
-                continue;
+                accounts[transaction.FromAccount] = 0;
             }
+            accounts[transaction.FromAccount] -= Convert.ToDouble(transaction.Amount);
             
-            if (!accounts.ContainsKey(fields[1]))
+            if (!accounts.ContainsKey(transaction.ToAccount))
             {
-                accounts[fields[1]] = 0;
+                accounts[transaction.ToAccount] = 0;
             }
-            accounts[fields[1]] -= Convert.ToDouble(fields[4]);
-            
-            if (!accounts.ContainsKey(fields[2]))
-            {
-                accounts[fields[2]] = 0;
-            }
-            accounts[fields[2]] += Convert.ToDouble(fields[4]);
-
+            accounts[transaction.ToAccount] += Convert.ToDouble(transaction.Amount);
         }
-
+        
         foreach (var kvp in accounts)
         {
             Console.WriteLine($"{kvp.Key}: {kvp.Value}");
