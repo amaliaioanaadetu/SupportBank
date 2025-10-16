1675902a234763729bf8979893b891552f4c9818
AmaAde <Amalia.Adetu@softwire.com>
2025-10-16 15:59:58 +0300

    Part 1

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Globalization;
+using System.IO;
 
-Console.WriteLine("Hello, World!");
+class SupportBank
+{
+    static void Main()
+    {
+        Dictionary<string, Double> accounts = new Dictionary<string, Double>();
+        
+        using var reader = new StreamReader("Transactions2014.csv");
+        string line;
+        bool isFirstLine = true;
+        while ((line = reader.ReadLine()) != null)
+        {
+            var fields = line.Split(',');
+            if (isFirstLine)
+            {
+                isFirstLine = false;
+                continue;
+            }
+            
+            if (!accounts.ContainsKey(fields[1]))
+            {
+                accounts[fields[1]] = 0;
+            }
+            accounts[fields[1]] -= Convert.ToDouble(fields[4]);
+            
+            if (!accounts.ContainsKey(fields[2]))
+            {
+                accounts[fields[2]] = 0;
+            }
+            accounts[fields[2]] += Convert.ToDouble(fields[4]);
+
+        }
+
+        foreach (var kvp in accounts)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+    }
+}
